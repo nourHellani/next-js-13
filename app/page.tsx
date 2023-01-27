e029ea26c94491e644a91c1f8643cf35308900c5
nourHellani <nourelhellani@gmail.com>
2023-01-27 12:38:27 +0200

    test

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link';
+
+function Home () {
+  return (
+    <div>
+        <h1 className="text-center text-[#333] p-5">Homepage</h1>
+        
+      </div>
+  )
+}
+
+export default Home; 
