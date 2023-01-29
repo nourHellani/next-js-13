a3ba076d45178761efcaa6d913cd91f69dd409fe
nourHellani <nourelhellani@gmail.com>
2023-01-29 22:02:04 +0200

    test

---
+++ .history/app/(users)/todos/TodosList_20230129220116.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Todo } from '../../../typings';
+import Link from 'next/link';
+const fetchTodos = async () => {
+    const timeout = Math.floor(Math.random() * 5 + 1) * 1000;
+    await new Promise((resolve) => setTimeout(resolve, timeout));
+
+
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+    const todos: Todo[] = await res.json();
+    //console.log('This is the todos',todos);
+    return todos;
+}
+async function TodosList() {
+    const todos = await fetchTodos()
+    return <>
+        {todos.map((todo) => (
+            <p key={todo.id}>
+                <Link href={'/todos/' + todo.id}> Todo:{todo.id}</Link>
+            </p>
+        ))
+        }
+    </>;
+
+}
+
+export default TodosList
---
+++ .history/app/(users)/todos/[todoId]/page_20230129220153.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { Todo } from "../../../../typings";
+import { notFound } from "next/navigation";
+
+export const dynamicParams = true;
+
+type PageProps = {
+    params: {
+        todoId: string;
+    };
+};
+
+const fetchTodo = async (todoId:string) => {
+
+
+    const res = await fetch(
+        
+        'https://jsonplaceholder.typicode.com/todos/'+todoId //run on local host 
+      // 'https://jsonplaceholder.typicode.com/todos/${todoId}' //didnt on local host 
+        
+        , 
+    {next : {revalidate:60 }}
+    );
+
+    const todo: Todo = await res.json();
+
+    return todo;
+}
+
+
+async function TodoPage({ params : {todoId}} : PageProps) {    
+
+    const todo = await fetchTodo(todoId);
+  
+
+    if (!todo.id) return notFound()
+
+
+    return (
+<>
+    <div className="p-10 bg-yellow-200 border-2 m-2 shadow-lg"> 
+        <p> 
+            #{todo.id}    
+
+            : {todo.title}   
+        </p>
+
+
+        <p>
+             completed: {todo.completed? "yes" : "no"}
+        </p>
+
+        <p className="border-t border-black mt-5 text-right">
+            by user: {todo.userId}
+        </p>
+
+    </div>
+    </>
+  )
+}
+
+export default TodoPage
+
+export async function generateStaticParams(){
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/');
+
+    const todos: Todo [] = await res.json();
+
+    const trimmedTodos = todos.splice(0,10);
+
+    return trimmedTodos.map(todo => ({
+        todoId : todo.id.toString(),
+    }))
+}
